3f4ba2403ad229349dc3f2ee0754905db48d0ab8
YONNER44 <destrucion99tremplo@outlook.com>
2026-01-14 16:44:52 -0500

    Cambios estilos arreglo para despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import react from "@astrojs/react"
 // https://astro.build/config
 export default defineConfig({
   site: "https://yonner44.github.io",
+  base: "/astro-sphere",
   trailingSlash: 'always',
   build: {
     format: 'directory'
