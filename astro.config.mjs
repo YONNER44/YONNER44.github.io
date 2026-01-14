6b7f9f00f067ae1b34cea2d412e9d238f2a940c1
YONNER44 <destrucion99tremplo@outlook.com>
2026-01-14 14:12:50 -0500

    Migración inicial a dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@ import { defineConfig } from "astro/config"
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
-import solidJs from "@astrojs/solid-js"
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-sphere-demo.vercel.app",
-  integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
+  site: "https://yonner44.github.io",
+  trailingSlash: 'always',
+  build: {
+    format: 'directory'
+  },
+  integrations: [mdx(), sitemap(), react(), tailwind({ applyBaseStyles: false })],
   devToolbar: {
     enabled: false
   }
